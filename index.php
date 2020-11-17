a29ff058584f521c0b63b5cc141bc62d7f00b50d
Jihoon Seo <46767780+jihoon-seo@users.noreply.github.com>
2020-11-17 16:42:47 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ echo '<pre>';
 // Outputs all the result of shellcommand "ls", and returns
 // the last output line into $last_line. Stores the return value
 // of the shell command in $retval.
-$last_line = system('./cpuload.sh 10 10', $retval);
+$last_line = system('./cpuload.sh 80 100', $retval);
 //$last_line = system('for ((i=0;i<=1000000;i++)); do; done;', $retval);
 //$last_line = system('stress --cpu 1 --timeout 10', $retval);
 
